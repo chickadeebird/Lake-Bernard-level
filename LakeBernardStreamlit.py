c76e037c377a7d57c25c2ddbbada9de5e45e92ff
chickadeebird <125906370+chickadeebird@users.noreply.github.com>
2023-07-08 11:27:49 -0400

    Update LakeBernardStreamlit.py

--- LakeBernardStreamlit.py
+++ LakeBernardStreamlit.py
@@ -402,7 +402,7 @@ def get_historical_level_data():
     df['Year'] = year
     df['Day'] = df['Day'].astype(int)
     df['Date'] = compose_date(df['Year'], days=df['Day'])
-    df['Date'].iloc(-1) = df['Date'].iloc(-2)
+    df.loc[-1,'Date'] = df.loc[-2,'Date']
     df['Month'] = df['Date'].dt.strftime('%b')
 
     return df
