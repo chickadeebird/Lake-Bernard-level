232bb0cc36022559eab159b9429bd86d2ee93afc
chickadeebird <125906370+chickadeebird@users.noreply.github.com>
2023-11-29 13:14:43 -0500

    Add files via upload
    Changed line colours

--- LakeBernardStreamlitMNR.py
+++ LakeBernardStreamlitMNR.py
@@ -473,14 +473,14 @@ y_list = df_precip['Total Precip (mm)'].to_list()
 # create plotly plots
 fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.01, row_heights=[0.8, 0.2])
 
-fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Top of High'], name='Top of High', line_color='Blue'), row=1, col=1)
-fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Top of Normal'], name='Top of Normal', line_color='Blue'), row=1, col=1)
-fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Bottom of Low'], name='Bottom of Low', line_color='Blue'), row=1, col=1)
-fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Bottom of Normal'], name='Bottom of Normal', line_color='Blue'), row=1, col=1)
-fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Target'], name='Target', line_color='Blue'), row=1, col=1)
+fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Top of High'], name='Top of High', line_color='Red'), row=1, col=1)
+fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Top of Normal'], name='Top of Normal', line_color='Yellow'), row=1, col=1)
+fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Bottom of Low'], name='Bottom of Low', line_color='Red'), row=1, col=1)
+fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Bottom of Normal'], name='Bottom of Normal', line_color='Yellow'), row=1, col=1)
+fig.append_trace(go.Line(x=df_historical['Date'], y=df_historical['Target'], name='Target', line_color='Green'), row=1, col=1)
 
 fig.append_trace(go.Scatter(x=day_list, y=group_list, name='Current', marker=dict(
-    color='Red',
+    color='Blue',
     size=4,
 )), row=1, col=1)
 
